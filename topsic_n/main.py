d7b4d96242978453e8127e0266497a1fbc53c730
yukaidesu <yukai_tsutsumi@pj100.biz>
2022-05-11 15:15:42 +0900

    凸

---
+++ topsic_n/main.py
@@ -0,0 +1,22 @@
+# 凸
+
+n = int(input())
+a = list(map(int, input().split()))
+ans =[False for _ in range(n)]
+
+for i in range(1, n - 1):
+    if (a[i] > a[i - 1]) and (a[i] > a[i + 1]):
+        ans[i] = True
+    else:
+        if a[i - 1] > a[i + 1]:
+            if (a[i - 1] - a[i + 1]) / 2 < (a[i] - a[i + 1]):
+                ans[i] = True
+
+trueans = "NO"
+for i in range(1, n - 1):
+    if ans[i] == True:
+        trueans = "YES"
+    else:
+        trueans = "NO"
+
+print(trueans)
