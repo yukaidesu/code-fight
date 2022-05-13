85dd9315f1e54c8d8dd8c53315842245b53148b7
yukaidesu <yukai_tsutsumi@pj100.biz>
2022-05-13 13:59:02 +0900

    三角形の相似

--- topsic_l/main.py
+++ topsic_l/main.py
@@ -8,14 +8,24 @@ for i in range(3):
     for j in range(2):
         if xy[i][j] > xy2[i][j]:
             if xy2[i][j] != 0:
-                #print(f"{xy[i][j]}ワル{xy2[i][j]}あまりがゼロ")
                 if xy[i][j] % xy2[i][j] != 0:
                     ans = False
+                else:
+                    #print(f"{xy[i][j]}ワル{xy2[i][j]}あまりがゼロ")
+                    if xy2[i][j] >= 0 and xy[i][j] < 0:
+                        ans = False
+                    elif xy[i][j] >= 0 and xy2[i][j] < 0:
+                        ans = False
         else:
             if xy[i][j] != 0:
-                #print(f"{xy2[i][j]}ワル{xy[i][j]}あまりがゼロ")
                 if xy2[i][j] % xy[i][j] != 0:
                     ans = False
+                else:
+                    #print(f"{xy[i][j]}ワル{xy2[i][j]}あまりがゼロ")
+                    if xy2[i][j] >= 0 and xy[i][j] < 0:
+                        ans = False
+                    elif xy[i][j] >= 0 and xy2[i][j] < 0:
+                        ans = False
 
 if ans:
     print("YES")
