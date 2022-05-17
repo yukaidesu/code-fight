8a509761328f80782cc14fc7eeddea8f98fd25dc
yukaidesu <yukai_tsutsumi@pj100.biz>
2022-05-17 10:35:03 +0900

    三角形の相似

--- topsic_l/main.py
+++ topsic_l/main.py
@@ -1,33 +1,33 @@
 #三角形の相似
-
-xy = [list(map(int, input().split())) for _ in range(3)]
+# 解答まち
+import itertools
+xy1 = [list(map(int, input().split())) for _ in range(3)]
 xy2 = [list(map(int, input().split())) for _ in range(3)]
 
-ans = True
-for i in range(3):
-    for j in range(2):
-        if xy[i][j] > xy2[i][j]:
-            if xy2[i][j] != 0:
-                if xy[i][j] % xy2[i][j] != 0:
-                    ans = False
-                else:
-                    #print(f"{xy[i][j]}ワル{xy2[i][j]}あまりがゼロ")
-                    if xy2[i][j] >= 0 and xy[i][j] < 0:
-                        ans = False
-                    elif xy[i][j] >= 0 and xy2[i][j] < 0:
-                        ans = False
-        else:
-            if xy[i][j] != 0:
-                if xy2[i][j] % xy[i][j] != 0:
-                    ans = False
-                else:
-                    #print(f"{xy[i][j]}ワル{xy2[i][j]}あまりがゼロ")
-                    if xy2[i][j] >= 0 and xy[i][j] < 0:
-                        ans = False
-                    elif xy[i][j] >= 0 and xy2[i][j] < 0:
-                        ans = False
+xy1_comb = itertools.combinations(xy1, 2)
+lines_1 = []
+for i in xy1_comb:
+    #print(i)
+    lines_1.append(((i[0][0] - i[1][0]) ** 2 + (i[0][1] - i[1][1]) ** 2) ** 0.5)
 
-if ans:
+xy2_comb = itertools.combinations(xy2, 2)
+lines_2 = []
+for i in xy2_comb:
+    #print(i)
+    lines_2.append(((i[0][0] - i[1][0]) ** 2 + (i[0][1] - i[1][1]) ** 2) ** 0.5)
+
+lines_1.sort()
+lines_2.sort() # sortすることでどの辺とどの辺を比べればいいかわかる
+
+#print(lines_1)
+#print(lines_2)
+
+rates = [] # それぞれの比率
+for i in range(3):
+    rates.append(lines_1[i] / lines_2[i])
+#print(rates)
+
+if rates[0] == rates[1] and rates[1] == rates[2]:
     print("YES")
 else:
     print("NO")
